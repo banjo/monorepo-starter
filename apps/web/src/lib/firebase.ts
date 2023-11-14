c9f7c74d223ef227632e46a015c2ee5fddc658ff
Anton Ödman <anton.odman@gmail.com>
2023-11-14 19:03:27 +0100

    chore: do not save firebase config in plain text

--- apps/web/src/lib/firebase.ts
+++ apps/web/src/lib/firebase.ts
@@ -1,14 +1,14 @@
+import { raise } from "@banjoanton/utils";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBBnJEQY3zZatM2UBPJsVKh2r0dBDhIZlU",
-    authDomain: "banjotest.firebaseapp.com",
-    projectId: "banjotest",
-    storageBucket: "banjotest.appspot.com",
-    messagingSenderId: "311195903232",
-    appId: "1:311195903232:web:86a702445bc7d7be3fbfe5",
-}; // TODO: Add firebase config
+const getFirebaseConfig = () => {
+    const key =
+        import.meta.env.VITE_FIREBASE_CONFIG ?? raise("VITE_FIREBASE_CONFIG is not defined");
 
-export const app = initializeApp(firebaseConfig);
+    const config = atob(key);
+    return JSON.parse(config);
+};
+
+export const app = initializeApp(getFirebaseConfig());
 export const auth = getAuth(app);
